47f5ff91a7ab60f0fc22998c992db0ba14671c4c
PhucNPH <126009035+se183119@users.noreply.github.com>
2024-01-18 16:41:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
+// Join a voice-channel
+bot.on("messageCreate", (msg) => {
+  if (msg.content === "!join") {
+    const voiceChannel = msg.member.voiceState.channelID;
+
+    if (voiceChannel) {
+      bot.joinVoiceChannel(voiceChannel).then((connection) => {
+        console.log(`Bot joined ${voiceChannel}`);
+      }).catch((error) => {
+        console.error(error);
+      });
+    } else {
+      msg.channel.createMessage("You need to be in a voice channel to use this command.");
+    }
+  }
+});
+
 bot.connect(); // Get the bot to connect to Discord
